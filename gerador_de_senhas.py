69fcbd893d06b51234e469ac1f6358af0df642c8
Samuel Henrique <152412378+shmuelhenrik@users.noreply.github.com>
2025-02-23 14:01:28 -0400

    Delete gerador_de_senhas.py

--- gerador_de_senhas.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-letras = string.ascii_letters
-numeros = string.digits
-caracteres_especiais = string.punctuation
-caracteres = letras + numeros + caracteres_especiais
-tamanho =int(input("Digite o tamanho da senha: "))
-senha = ''.join(random.choice(caracteres) for i in range(tamanho))
-print(f"Senha:{senha}")
